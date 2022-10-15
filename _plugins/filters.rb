13df09698b05f2454ecaf59da39f8462a7c88024
maemon <47819358+maemon4095@users.noreply.github.com>
2022-10-15 14:47:04 +0900

    pretty_jsonify

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,16 +1,22 @@
-module Jekyll
-    module AdditionalFilters
-        def escape_path(input)
-            Jekyll::URL.escape_path(Jekyll::Utils.slugify(input))
-        end
-        module_function :escape_path
-        public :escape_path
-        def unescape_path(input)
-            Jekyll::URL.unescape_path(input)
-        end
-        module_function :unescape_path
-        public :unescape_path
+require 'json'
+
+module AdditionalFilters
+    def escape_path(input)
+        Jekyll::URL.escape_path(Jekyll::Utils.slugify(input))
     end
+    module_function :escape_path
+    public :escape_path
+    def unescape_path(input)
+        Jekyll::URL.unescape_path(input)
+    end
+    module_function :unescape_path
+    public :unescape_path
+
+    def pretty_jsonify(input)
+        JSON.pretty_generate(input)
+    end
+    module_function :pretty_jsonify
+    public :pretty_jsonify
 end
   
-Liquid::Template.register_filter(Jekyll::AdditionalFilters)
+Liquid::Template.register_filter(AdditionalFilters)
